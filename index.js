65c98474a2e880cec0dc8a184f5e925cbe1a2a89
venkyu <44948813+venkyu@users.noreply.github.com>
2025-12-19 23:59:19 -0800

    Update crypt_secret to 30 characters

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const authConfig = {
   "refresh_token": "1//04yz6XzAyT1dqCgYIARAAGAQSNwF-L9IrW9RhT_f_1stUmfa8OZ_DN5O7zcbcCj5IendEb0iSD0dtx7MqgXMfS74bXEOu9-bqUyM", // Refresh Token
   "folder_list_url": "1fHta6VyIVoT_TOzVod-aXkDTu4U3_xpR", // Folder Id List(Only if you have any folders in roots) - Sample: https://cdn.jsdelivr.net/gh/cheems/goindex-extended@master/sample-files/sample-folder-id-list.txt
   // Crypt Secret must be 32 characters long - Sample: "1234567890123456abcdefghIJKLMN!*" | don't use these characters (\,/,",') 
-  "crypt_secret": "utvdriveindexsecretkey1234567890", // Crypt Secret (Required)* This is used to encrypt file ids
+  "crypt_secret": "utvdriveindexsecretkey123456789", // Crypt Secret (Required)* This is used to encrypt file ids
   /**
     * Set up multiple Drives to be displayed; add multiples by format
     * [id]: It can be team folder id, subfolder id, or "root" (representing the root directory of personal disk);
